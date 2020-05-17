813f255e067443d791040f4fc298d8fb3ff06684
Jason Lee <jasonepic0801@gmail.com>
2020-05-17 20:31:56 +0800

    more words

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var wordBank = {
-  unguessed: ["Kimi No Nawa", "Weathering with You", "Stein's Gate", "Demon Slayer", "Vinland Saga", "Glepnir", "Mirai Nikki", "Kaguya-sama: Love is War", "Attack on Titan", "Fullmetal Alchemist: Brotherhood", "Hyouka", "Made in Abyss", "Her Blue Sky", "The Promised Neverland", "I Don't Want to Get Hurt, so I'll Max Out My Defense", "Mahou Shoujo Madoka Magica"],
+  unguessed: ["Your Name", "Weathering with You", "Stein's Gate", "Demon Slayer", "Vinland Saga", "Glepnir", "Future Diary", "Kaguya-sama: Love is War", "Attack on Titan", "Fullmetal Alchemist: Brotherhood", "Hyouka", "Made in Abyss", "Her Blue Sky", "The Promised Neverland", "Puella Magi Madoka Magica", "Fate/Apocrypha", "Beastars", "Tower of God", "A Silent Voice", "Overlord", "Charlotte", "Violet Evergarden", "Nisekoi", "Rascal Does Not Dream of Bunny Girl Senpai", "Goblin Slayer", "Isekai Quartet", "The Saga of Tanya the Evil", "Sword Art Online: Alicization", "Devilman: Crybaby", "Blend S", "Chihayafuru", "Fate/Zero", "Kakegurui", "ERASED"],
   guessed: [],
   current: "",
   blanksLeft: 0,
