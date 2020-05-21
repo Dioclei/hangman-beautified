7d6cc85322821656bc4a05bf6d32ace39bb7ef0a
Jason Lee <jasonepic0801@gmail.com>
2020-05-21 15:53:17 +0800

    Added list of school-related items and places

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ let animeList = ["Your Name", "Weathering with You", "Stein's Gate", "Demon Slay
 
 let leagueChampions = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe", "Aurelion Sol", "Azir", "Bard", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", "Cho'Gath", "Corki", "Darius", "Diana", "Dr. Mundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern", "Janna", "Jarvan IV", "Jax", "Jayce", "Jhin", "Jinx", "Kaisa", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "Kha'Zix", "Kindred", "Kled", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus", "Neeko", "Nidalee", "Nocturne", "Sejuani", "Senna", "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Nunu", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", "Rek'Sai", "Renekton", "Rengar", "Riven", "Ryze", "Sona", "Soraka", "Swain", "Sylas", "Syndra", "Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vi", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo", "Yorick", "Yuumi", "Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra"]
 
+let schoolItems = ["Paper", "Pencil", "Google Chrome", "Macbook Pro", "Notebook", "Water Bottle", "Table", "Whiteboard", "Marker", "Highlighter", "Frisbee", "Printer", "Colour Pencil", "Paintbrush", "Charcoal Eraser", "Letter of Appreciation", "Certificate of Excellence", "Examination", "School Fees", "Pocket Money", "Threadmill", "Free Weights", "Macaroni and Cheese", "Ham and Cheese Sandwich", "Mackerel Curry Rice", "Ice Milo", "Auditorium", "Android Studio"]
+
 var wordBank = {
-  unguessed: leagueChampions.slice(0),
+  unguessed: schoolItems.slice(0),
   guessed: [],
   current: "",
   blanksLeft: 0,
